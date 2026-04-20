d7c2a0b8e3dbbc431e1115d560d00f1d19677117
Ankit <ankitroytitan@gmail.com>
2026-04-20 12:50:22 +0530

    Commit fron end

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,6 @@ from src.exception import CustomException
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
@@ -23,15 +22,16 @@ def save_object(file_path, obj):
 def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
+        best_models = {}
 
         for name, model in models.items():
             para = param[name]
 
-            # Grid Search
+            # 🔥 GridSearchCV
             gs = GridSearchCV(model, para, cv=3)
             gs.fit(X_train, y_train)
 
-            # Best model
+            # ✅ Get best model directly
             best_model = gs.best_estimator_
 
             # Predictions
@@ -43,8 +43,9 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             test_score = r2_score(y_test, y_test_pred)
 
             report[name] = test_score
+            best_models[name] = best_model
 
-        return report
+        return report, best_models
 
     except Exception as e:
         raise CustomException(e, sys)
